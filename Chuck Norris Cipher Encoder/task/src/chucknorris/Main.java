0c12c2f7f28abb5dcb67d4fa00c3cc834049e9bc
AHMEDHOSNY <ahhosnyas@gmail.com>
2022-11-29 00:07:43 +0200

    Stage 1/5: Analyze the words

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -1,7 +1,21 @@
 package chucknorris;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Input string:");
+
+        String input = scanner.nextLine();
+        System.out.println();
+
+        StringBuilder sb = new StringBuilder();
+        for (var c : input.toCharArray()) {
+            sb.append(c).append(" ");
+        }
+
+        System.out.println(sb.toString());
     }
 }
