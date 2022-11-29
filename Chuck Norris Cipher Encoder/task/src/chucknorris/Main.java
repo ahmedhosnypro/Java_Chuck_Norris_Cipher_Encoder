c84bd7f30258d67fff2b7959accd36bc9d388855
AHMEDHOSNY <ahhosnyas@gmail.com>
2022-11-29 12:40:35 +0200

    Stage 3/5: Chuck Norris encrypts only with zeros

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -14,10 +14,48 @@ public class Main {
                 The result:
                 """);
 
-
+        StringBuilder binary = new StringBuilder();
         for (var c : input.toCharArray()) {
-            System.out.print(c + " = ");
-            System.out.print(String.format("%7s%n", Integer.toBinaryString(c)).replace(" ", "0"));
+            binary.append(toBinaryString(c));
         }
+
+        String chuckNorris = toChuckNorris(binary.toString());
+        System.out.println(chuckNorris);
+    }
+
+    private static String toBinaryString(char c) {
+        return String.format("%7s", Integer.toBinaryString(c)).replace(" ", "0");
+    }
+
+    private static String toChuckNorris(String binary) {
+
+        StringBuilder chuckNorris = new StringBuilder();
+
+        int loopNumber = binary.charAt(0) - '0';
+        int blockLength = 1;
+
+        char[] binaries = binary.toCharArray();
+
+        for (int i = 1; i < binaries.length; i++) {
+            if (binaries[i] - '0' == loopNumber) {
+                blockLength++;
+            } else {
+                if (loopNumber == 0) {
+                    chuckNorris.append("00 ").append("0".repeat(blockLength)).append(" ");
+                } else {
+                    chuckNorris.append("0 ").append("0".repeat(blockLength)).append(" ");
+                }
+
+                loopNumber = binaries[i] - '0';
+                blockLength = 1;
+            }
+        }
+
+        if (loopNumber == 0) {
+            chuckNorris.append("00 ").append("0".repeat(blockLength)).append(" ");
+        } else {
+            chuckNorris.append("0 ").append("0".repeat(blockLength)).append(" ");
+        }
+        return chuckNorris.toString();
     }
 }
